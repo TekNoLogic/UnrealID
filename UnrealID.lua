73ed1187536336947bdef65d688415388abee259
tekkub <tekkub@gmail.com>
2010-06-27 13:49:17 -0600

    That should be nil

--- UnrealID.lua
+++ UnrealID.lua
@@ -6,7 +6,7 @@ ns:RegisterEvent("ADDON_LOADED")
 function ns:ADDON_LOADED(event, addon)
 	if addon ~= myname then return end
 
-	LibStub("tekKonfig-AboutPanel").new(myfullname, myname) -- Make first arg nil if no parent config panel
+	LibStub("tekKonfig-AboutPanel").new(nil, myname)
 
 	self:UnregisterEvent("ADDON_LOADED")
 	self.ADDON_LOADED = nil
