bdfe690e92b395deeab05a7c04a08e566760379b
tekkub <tekkub@gmail.com>
2010-06-27 21:18:57 -0600

    gah

--- chat.lua
+++ chat.lua
@@ -2,13 +2,17 @@
 local myname, ns = ...
 
 
-local function bettername(self, event, msg, name, _, _, _, status, _, _, _, _, arg11) -- arg13 is the presenceID, which isn't passed right now
+local function bettername(self, event, ...)
+	ns.Debug("Chat filter", event, ...)
+
+	local msg, name, _, _, _, status, _, _, _, _, arg11 = ... -- arg13 is the presenceID, which isn't passed right now
+
 	local chattype = event == "CHAT_MSG_BN_WHISPER" and "BN_WHISPER" or "BN_WHISPER_INFORM"
 	local chatGroup, chatTarget = Chat_GetChatCategory(chattype), name:upper()
 
 	local flag, stamp = status and status ~= '' and _G["CHAT_FLAG_"..status] or "", CHAT_TIMESTAMP_FORMAT and BetterDate(CHAT_TIMESTAMP_FORMAT, time()) or ""
 	msg = gsub(msg, "%%", "%%%%")
-	local body = stamp.. format(_G["CHAT_"..chattype.."_GET"]..msg, flag.."|HBNplayer:"..name..":"..ns.PIDs[name]..":"..arg11..":"..chatGroup..(chatTarget and ":"..chatTarget or "").."|h".."["..ns.names[name].."]".."|h")
+	local body = stamp.. format(_G["CHAT_"..chattype.."_GET"]..msg, flag.."|HBNplayer:"..name..":"..ns.pIDs[name]..":"..arg11..":"..chatGroup..(chatTarget and ":"..chatTarget or "").."|h".."["..ns.names[name].."]".."|h")
 
 	local accessID, typeID, info = ChatHistory_GetAccessID(chatGroup, chatTarget), ChatHistory_GetAccessID(chattype, chatTarget), ChatTypeInfo[chattype]
 	self:AddMessage(body, info.r, info.g, info.b, info.id, false, accessID, typeID)
