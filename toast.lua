93e7a2e6f6e92ff957de60db5466db7f4a91dbae
tekkub <tekkub@gmail.com>
2010-06-28 01:01:23 -0600

    Better toasts

--- toast.lua
+++ toast.lua
@@ -2,5 +2,11 @@
 local myname, ns = ...
 
 
-local o = BNToastFrameTopLine.SetFormattedText
-function BNToastFrameTopLine:SetFormattedText(format, ...) return o(self, "%s", ...) end
+hooksecurefunc("BNToastFrame_Show", function()
+	local toastType, toastData = BNToastFrame.toastType, BNToastFrame.toastData
+	if not toastType or toastType < 1 or toastType > 3 then return end
+
+	local _, givenName, surname, _, _, _, _, _, _, _, _, note = BNGetFriendInfoByID(toastData)
+	local name = ns.names[givenName.." "..surname]
+	if name then BNToastFrameTopLine:SetText(name) end
+end)
