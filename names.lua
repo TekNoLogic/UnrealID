a1e6a09ccd458091ca5a777064bc382c70323767
tekkub <tekkub@gmail.com>
2010-07-10 23:00:24 -0600

    Better handling on name/pID tables

--- names.lua
+++ names.lua
@@ -3,13 +3,19 @@ local myname, ns = ...
 
 
 ns.names = setmetatable({}, {__index = function(t,i)
-	local _, online = BNGetNumFriends()
-	for j=1,online do
-		local _, givenName, surname, _, _, _, _, _, _, _, _, note = BNGetFriendInfo(j)
-		if (givenName.. " ".. surname) == i then
-			local v = note and note ~= "" and note or givenName
-			t[i] = v
-			return v
+	if type(i) == "number" then
+		local _, givenName, surname, _, _, _, _, _, _, _, _, note = BNGetFriendInfoByID(toastData)
+		local v = note ~= "" and note or givenName
+		t[i] = v
+		return v
+	else
+		for j=1,BNGetNumFriends() do
+			local _, givenName, surname, _, _, _, _, _, _, _, _, note = BNGetFriendInfo(j)
+			if (givenName.. " ".. surname) == i then
+				local v = note ~= "" and note or givenName
+				t[i] = v
+				return v
+			end
 		end
 	end
 	return i
@@ -27,7 +33,23 @@ ns.pIDs = setmetatable({}, {__index = function(t,i)
 end})
 
 
-ns:RegisterEvent("BN_DISCONNECTED")
-function ns:BN_DISCONNECTED()
-	table.wipe(ns.pIDs)
+function ns:BN_CONNECTED(...)
+	if BNConnected() then
+		for i=1,BNGetNumFriends() do
+			local pID, givenName, surname, _, _, _, _, _, _, _, _, note = BNGetFriendInfo(i)
+			local k = givenName.. " ".. surname
+			ns.names[k], ns.pIDs[k] = (note ~= '' and note) or givenName, pID
+			ns.names[pID] = ns.names[k]
+		end
+	else
+		table.wipe(ns.names)
+		table.wipe(ns.pIDs)
+	end
 end
+ns.BN_DISCONNECTED = ns.BN_CONNECTED
+ns.PLAYER_LOGIN = ns.BN_CONNECTED
+ns.BN_SELF_ONLINE = ns.BN_CONNECTED
+ns:RegisterEvent("BN_CONNECTED")
+ns:RegisterEvent("BN_DISCONNECTED")
+ns:RegisterEvent("BN_SELF_ONLINE")
+ns:RegisterEvent("PLAYER_LOGIN")
--- toast.lua
+++ toast.lua
@@ -6,7 +6,5 @@ hooksecurefunc("BNToastFrame_Show", function()
 	local toastType, toastData = BNToastFrame.toastType, BNToastFrame.toastData
 	if not toastType or toastType < 1 or toastType > 3 then return end
 
-	local _, givenName, surname, _, _, _, _, _, _, _, _, note = BNGetFriendInfoByID(toastData)
-	local name = ns.names[givenName.." "..surname]
-	if name then BNToastFrameTopLine:SetText(name) end
+	BNToastFrameTopLine:SetText(ns.names[toastData])
 end)
